603b74b3016e7b9de066e19cff0b927791805aad
Nan Santos <pixelado405@gmail.com>
2025-11-03 22:25:54 -0300

    Início das aulas de JS!
    Conteúdo do dia 03/11/2025

---
+++ Aula 001/app.js
@@ -0,0 +1,92 @@
+// Boas práticas
+    /*
+        Não declarar variáveis com var;
+        Nomes descritivos e significativos;
+        Funções não muito longas;
+        Utilizar const sempre que possível ao invés de let;
+        Utilizar === para comparações;
+        Variáveis sempre com um valor declarado;
+        Sempre utilizar ponto e vírgula;
+        Utilizar o operador ternário sempre que possível ao invés de if;
+        Preferir For Loop ao invés de forEach;
+        Evitar o "new" por questões de performance;
+        Declarar funções com expressões;
+        Funções devem ter apenas um papel;
+        Sempre declarar um valor dentro do parâmetro de uma função;
+        Realizar testes unitários;
+    */
+// Case sensitive
+    /*
+        JavaScript é uma linguagem sensível a letras maiúsculas e minúsculas, diferenciando ambas.
+    */
+
+    console.log("Olá mundo!")
+
+// Sintaxe básica e comentário
+    /*
+        // Uma linha de comentário
+        /*
+           Várias linhas de comentário 
+        */
+    /*
+        Case-sensitive, utiliza o conjunto de caracteres Unicode;
+        Instruções são chamadas de declaração e são separadas por ponto e vírgula (;);
+        Espaços, tabulação e uma nova linha são chamados de espaços em branco;
+    */
+// Declaração de variáveis(var, let, const)
+
+let nome = "Nan"
+console.log("Olá " + nome)
+nome = "Santos"
+console.log(nome)
+const idade = 39
+console.log(idade)
+
+// Console e Debug
+
+console.warn("Atenção")
+console.info("Informações")
+console.error("Erro")
+
+//Tipos de dados
+    /*
+        Número - number;
+        Inteiro - int;
+        Real - float;
+        Booleano - bool;
+        Texto - string;
+    */
+
+//string
+
+let cidade = "Americana"
+
+//number
+
+let salario = 2000
+
+//bool
+
+let fumante = false
+
+console.log(typeof cidade)
+console.log(typeof salario)
+console.log(typeof fumante)
+
+// Operadores
+//     Operador Atribuição
+
+//     Operadores aritméticos
+
+//     Operadores relacionais
+
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+
+//     Laços de repetição
+
+// Arrays --> vetor
+
+// Funções personalizadas
