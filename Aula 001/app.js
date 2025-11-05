25f07329a0e05710cd0eee19aedb8f1a7e454066
Nan Santos <pixelado405@gmail.com>
2025-11-05 19:56:31 -0300

    Operadores!

--- Aula 001/app.js
+++ Aula 001/app.js
@@ -74,13 +74,46 @@ console.log(typeof salario)
 console.log(typeof fumante)
 
 // Operadores
-//     Operador Atribuição
+//     Operador Atribuição ( = )
 
-//     Operadores aritméticos
+salario = 2500
 
-//     Operadores relacionais
+//     Operadores aritméticos ( + , - , * , / , %)
 
-//     Operadores lógicos
+let n1 = 10, n2 = 5
+
+console.log(n1+n2) // Soma
+console.log(n1-n2) // Subtração
+console.log(n1*n2) // Multiplicação
+console.log(n1/n2) // Divisão
+console.log(n1%n2) // Resto
+
+//     Operadores relacionais (== , != , > , < , >=, <=)
+
+console.log(n1 == n2)
+console.log(n1 != n2)
+console.log(n1 > n2)
+console.log(n1 < n2)
+console.log(n1 >= n2)
+console.log(n1 <= n2)
+
+//     Operadores lógicos ( ! - não / NOT || - ou / OR && - e / AND)
+
+let altura = 1.2
+let peso = 80
+let tipo = "criança"
+
+// Muda o resultado final, se for
+
+console.log(!10>5)
+
+// Com || apenas uma verificação precisa ser verdadeira.
+
+console.log(altura > 1.5 || peso < 150 || tipo != "criança")
+
+// Com && todas as verificações devem ser verdadeiras para o resultado final ser verdadeiro.
+
+console.log(altura > 1.5 && peso < 150 && tipo != "criança")
 
 // Estrutura
 //     Estrutura de controle/decisão
