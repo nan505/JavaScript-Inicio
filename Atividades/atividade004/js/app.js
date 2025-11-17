b97c76d5a467f7cfb840f4218197f688ed676720
Nan Santos <pixelado405@gmail.com>
2025-11-17 19:06:12 -0300

    Mais filmes adicionados!

--- Atividades/atividade004/js/app.js
+++ Atividades/atividade004/js/app.js
@@ -1,7 +1,7 @@
-let filmes = ['filme01.jpg', 'filme02.jpg', 'filme03.jpg']
+let filmes = ['filme01.jpg', 'filme02.jpg', 'filme03.jpg', 'filme04.jpg', 'filme05.jpg', 'filme06.jpg']
 
 let contador = 0
-while (contador < 3){
+while (contador < 6){
     document.write("<img src='./img/" + filmes[contador] + "'>")
     contador = contador + 1
 }
